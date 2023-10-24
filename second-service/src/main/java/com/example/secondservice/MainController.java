843d8f52d8881ba7d643515990a8f1e3fc461659
mmmjj <djkls7@naver.com>
2023-10-24 17:14:38 +0900

    spring cloud gateway - second service - add filter api

--- second-service/src/main/java/com/example/secondservice/MainController.java
+++ second-service/src/main/java/com/example/secondservice/MainController.java
@@ -1,15 +1,21 @@
 package com.example.secondservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/second-service")
 public class MainController {
 
     @GetMapping("/welcome")
-    public String selcome () {
+    public String selcome() {
         return "Welcome to the second service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        return header;
     }
 }
