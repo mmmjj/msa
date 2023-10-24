9139f4a210a5bcdb477276bd7de99546866e5899
mmmjj <djkls7@naver.com>
2023-10-24 16:42:55 +0900

    spring cloud gateway - second service restContoroller

---
+++ second-service/src/main/java/com/example/secondservice/MainController.java
@@ -0,0 +1,15 @@
+package com.example.secondservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping("/welcome")
+    public String selcome () {
+        return "Welcome to the second service";
+    }
+}
