9b1eef0f856aefaaec269be76f4b22b3063d3969
mmmjj <djkls7@naver.com>
2023-10-24 17:14:12 +0900

    spring cloud gateway - first service - add filter api

--- first-service/src/main/java/com/example/firstservice/MainController.java
+++ first-service/src/main/java/com/example/firstservice/MainController.java
@@ -1,15 +1,21 @@
 package com.example.firstservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/first-service")
 public class MainController {
 
     @GetMapping("/welcome")
     public String selcome () {
         return "Welcome to the first service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        return header;
     }
 }
