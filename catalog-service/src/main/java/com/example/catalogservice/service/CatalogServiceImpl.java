35a6f20951b0de34fb756ae8df5d1d891b4f5458
mmmjj <djkls7@naver.com>
2023-10-26 10:00:59 +0900

    catalogs Microservice - 4 - service,impl 생성

---
+++ catalog-service/src/main/java/com/example/catalogservice/service/CatalogServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.catalogservice.service;
+
+import com.example.catalogservice.jpa.CatalogEntity;
+import com.example.catalogservice.jpa.CatalogRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+//@Data
+@Slf4j
+@Service
+@AllArgsConstructor
+public class CatalogServiceImpl implements CatalogService{
+
+    CatalogRepository catalogRepository;
+
+    /*@Autowired
+    public CatalogServiceImpl(CatalogRepository catalogRepository) {
+        this.catalogRepository = catalogRepository;
+    }*/
+
+    @Override
+    public Iterable<CatalogEntity> getAllCatalogs() {
+        return catalogRepository.findAll();
+    }
+}
