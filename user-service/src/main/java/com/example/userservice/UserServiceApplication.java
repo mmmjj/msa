cc9af1e8268da9ec37128bba0d6594de21814c30
mmmjj <djkls7@naver.com>
2023-11-03 16:08:39 +0900

    microservice 통신 - 2 userservice -> orderservice, loadBalanced로 ip대신 applicationname으로 호출

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
@@ -25,6 +26,7 @@ public class UserServiceApplication {
      * @return
      */
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
